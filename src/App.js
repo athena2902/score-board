1cc115af44f1c7d345e2aaba489d0d8cafe01633
Quyet Vu <quyetvv_55@vnu.edu.vn>
2019-01-08 11:10:45 +0700

    score board

--- src/App.js
+++ src/App.js
@@ -1,28 +1,74 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import Counter from './Counter';
 import './App.css';
 
+const Header = (props) =>  (
+    <header>
+        <h1>{props.title}</h1>
+        <span className="stats">Players: {props.totalPlayers}</span>
+    </header>
+)
+
+const Player = (props) => (
+    <div className="player">
+     <span className="player-name">
+         <button className="remove-player" onClick={ () => props.removePlayer(props.id) }>x</button>
+         {props.name}
+     </span>
+
+        <Counter score={props.score }/>
+    </div>
+)
+
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+    state = {
+        players: [
+            {
+                name: "Quyet",
+                id: 1
+            },
+            {
+                name: "Lucy",
+                id: 2
+            },
+            {
+                name: "Tu Anh",
+                id: 3
+            },
+            {
+                name: "Linh",
+                id: 4
+            },
+            {
+                name: "Taylor",
+                id: 5
+            },
+        ]
+    }
+    handleRemovePlayer = (id) => {
+        this.setState( prevState => {
+            return {
+                players: prevState.players.filter(p => p.id !== id)
+            }
+        })
+    }
+    render() {
+        return(
+            <div className="scoreboard">
+                <Header title="Quyet's Scoreboard" totalPlayers={this.state.players.length} />
+
+                {/* Players list */}
+                {this.state.players.map(player =>
+                    <Player
+                        name={player.name}
+                        id={player.id}
+                        key={player.id.toString()}
+                        removePlayer={this.handleRemovePlayer}
+                    />
+                )}
+            </div>
+        )
+    }
 }
 
 export default App;
